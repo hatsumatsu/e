8b034cf52e55ad6c86c201351cce7a34237227e5
Martin Wecke <>
2020-05-02 21:08:21 +0200

    added prettier to the project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-`````
+```
 _____________________________
 ___________/\/\/\/\/\/\______
 __________/\_________________
@@ -7,7 +7,7 @@ ________/\/\_________________
 _______/\/\/\/\/\/\__________
 _____________________________
 
-`````
+```
 
 Tiny DOM manipulation helper with a familiar fluent API.
 
@@ -15,26 +15,24 @@ Tiny DOM manipulation helper with a familiar fluent API.
 
 `npm install @superstructure.net/e`
 
-
 ### Usage
 
-`````
+```
 import E from @superstructure.net/e;
 
 // create collection
-new E( '.my-selector' );
-new E( '[data-module-role="button"]' );
-new E( elementOrNodeList );
-new E( '<div>' );
+new E('.my-selector');
+new E('[data-module-role="button"]');
+new E(elementOrNodeList);
+new E('<div>');
 
 // create collection and do stuff
-new E( '<div>' )
-  .setAttr( 'data-module-role', 'title' )
-  .setText( 'Groovy 🎷' )
-  .addClass( 'active' )
-  .appendTo( 'body' )
-`````
-
+new E('<div>')
+  .setAttr('data-module-role', 'title')
+  .setText('Groovy 🎷')
+  .addClass('active')
+  .appendTo('body')
+```
 
 ### API
 
@@ -43,78 +41,100 @@ new E( '<div>' )
 Filter the current collection by a selector or an index.
 
 ##### `find(selector)`
+
 Get children of the current collection matching `selector`.
 
 ##### `closest(selector)`
+
 Gets the closest parent matching `selector`.
 
 ##### `append(selector|Element|NodeList|E)`
+
 Append nodes to the current collection.
 
 ##### `appendTo(selector|Element|NodeList|E)`
+
 Append the current collection to the passed nodes.
 
-#####  `prepend(selector|Element|NodeList|E)`
+##### `prepend(selector|Element|NodeList|E)`
+
 Prepend nodes to the current collection.
 
 ##### `prependTo(selector|Element|NodeList|E)`
+
 Prepend the current collection to the passed nodes.
 
 ##### `clone()`
+
 Clone the collection.
 
 ##### `remove()`
+
 Remove the collection from DOM.
 
 ##### `addClass(class)`
+
 Add `class` to the collection.
 
 ##### `removeClass(class)`
+
 Remove `class` from the collection.
 
 ##### `toggleClass(class)`
+
 Toggle `class` on the collection.
 
 ##### `setAttr(key, value [,namespace])`
+
 Set attribute `key` to `value`. If `namespace` is passed the key used is `data-{namespace}-{key}`.
 
 ##### `getAttr(key [,namespace])`
+
 Get attribute value of `key`. If `namespace` is passed the key used is `data-{namespace}-{key}`.
 
 ##### `toggleAttr(key, values [,namespace])`
+
 Toggle attribute `key` between `values`. If `namespace` is passed the key used is `data-{namespace}-{key}`.
 `values` must be an array containing exactly two strings.
 
-
 ##### `setText(text)`
+
 Set text content of the first collection entry;
 
 ##### `getText()`
+
 Get text content of the first collection entry;
 
 ##### `setHTML(text)`
+
 Set inner HTML of the first collection entry;
 
 ##### `getHTML()`
+
 Get inner HTML of the first collection entry;
 
 ##### `css(syles)`
+
 Apply styles passed as a key-value-based object. Like `{fontSize:'2rem',color:'yellow'}`.
 
 ##### `nodes(index)` / `n(index)` / `get(index)`
+
 Get collection entry at `index`. Returns all collection entries if no index is passed.
 
 ##### `isEmpty()`
+
 Returns `true` if the collection is empty.
 
 ##### `repaint()`
+
 Force repaint on colection entries.
 
 ### Q&A
 
-#####  This looks a lot like JQ**** ...
+##### This looks a lot like JQ\*\*\*\* ...
+
 Don’t be silly, no one is using JQuery these days, right? 😇
 
 ##### Why not using Vanilly JS instead?
-You absolutely could but isn’t it nice to have a chainable API?
 
+You absolutely could but isn’t it nice to have a chainable API?
--- e.js
+++ e.js
@@ -16,454 +16,451 @@ with a familiar fluent API
  */
 
 export default class E {
-  constructor(target) {
-    /**
-     * new E()
-     * creates an empty collection
-     */
-    if (!target) {
-      this._nodes = [];
+    constructor(target) {
+        /**
+         * new E()
+         * creates an empty collection
+         */
+        if (!target) {
+            this._nodes = [];
+        }
+
+        /**
+         * new E( '<div>' )
+         * creates a collection with one <div> Element
+         *
+         * new E( '{selector}' )
+         * creates a collection containing the result of querySelectorAll( '{selector}' )
+         */
+        if (typeof target === 'string') {
+            if (target.charAt(0) === '<' && target.charAt(target.length - 1) === '>') {
+                this._nodes = [document.createElement(target.replace(/[<>]/gi, ''))];
+            } else {
+                this._nodes = document.querySelectorAll(target);
+            }
+        }
+
+        /**
+         * new E( {Element|NodeList} )
+         * creates a collection containing the passed nodes
+         */
+        if (typeof target === 'object') {
+            if (target.length === undefined) {
+                this._nodes = [target];
+            } else {
+                this._nodes = target;
+            }
+        }
+
+        return this;
     }
 
     /**
-     * new E( '<div>' )
-     * creates a collection with one <div> Element
-     *
-     * new E( '{selector}' )
-     * creates a collection containing the result of querySelectorAll( '{selector}' )
+     * Traversing
      */
-    if (typeof target === "string") {
-      if (
-        target.charAt(0) === "<" &&
-        target.charAt(target.length - 1) === ">"
-      ) {
-        this._nodes = [document.createElement(target.replace(/[<>]/gi, ""))];
-      } else {
-        this._nodes = document.querySelectorAll(target);
-      }
+    filter(selector) {
+        if (!this._nodes.length) {
+            return this;
+        }
+
+        if (selector === undefined) {
+            return this;
+        }
+
+        if (typeof selector === 'string') {
+            return new E(
+                Array.from(this._nodes).filter((node) => {
+                    return node.matches(selector);
+                })
+            );
+        }
+
+        if (typeof selector === 'number') {
+            if (!this._nodes[selector]) {
+                return new E();
+            }
+
+            return new E(this._nodes[selector]);
+        }
+
+        return this;
+    }
+
+    find(selector) {
+        if (!this._nodes.length) {
+            return this;
+        }
+
+        if (!selector) {
+            return this;
+        }
+
+        return new E(this._nodes[0].querySelectorAll(selector));
+    }
+
+    closest(selector) {
+        if (!this._nodes.length) {
+            return this;
+        }
+
+        if (!selector) {
+            return this;
+        }
+
+        return new E(this._nodes[0].closest(selector));
     }
 
     /**
-     * new E( {Element|NodeList} )
-     * creates a collection containing the passed nodes
+     * Manipulating
      */
-    if (typeof target === "object") {
-      if (target.length === undefined) {
-        this._nodes = [target];
-      } else {
-        this._nodes = target;
-      }
+    append(children) {
+        if (!this._nodes.length) {
+            return this;
+        }
+
+        if (!children) {
+            return this;
+        }
+
+        children = new E(children);
+
+        if (!children) {
+            return this;
+        }
+
+        this._nodes.forEach((node) => {
+            children.nodes().forEach((child) => {
+                node.append(child);
+            });
+        });
+
+        return this;
     }
 
-    return this;
-  }
+    appendTo(parents) {
+        if (!this._nodes.length) {
+            return this;
+        }
 
-  /**
-   * Traversing
-   */
-  filter(selector) {
-    if (!this._nodes.length) {
-      return this;
+        if (!parents) {
+            return this;
+        }
+
+        parents = new E(parents);
+
+        if (!parents) {
+            return this;
+        }
+
+        this._nodes.forEach((node) => {
+            parents.nodes().forEach((parent) => {
+                parent.append(node);
+            });
+        });
+
+        return this;
     }
 
-    if (selector === undefined) {
-      return this;
+    prepend(children) {
+        if (!this._nodes.length) {
+            return this;
+        }
+
+        if (!children) {
+            return this;
+        }
+
+        children = new E(children);
+
+        if (!children) {
+            return this;
+        }
+
+        this._nodes.forEach((node) => {
+            children.nodes().forEach((child) => {
+                node.prepend(child);
+            });
+        });
+
+        return this;
     }
 
-    if (typeof selector === "string") {
-      return new E(
-        Array.from(this._nodes).filter((node) => {
-          return node.matches(selector);
-        })
-      );
+    prependTo(parents) {
+        if (!this._nodes.length) {
+            return this;
+        }
+
+        if (!parents) {
+            return this;
+        }
+
+        parents = new E(parents);
+
+        if (!parents) {
+            return this;
+        }
+
+        this._nodes.forEach((node) => {
+            parents.nodes().forEach((parent) => {
+                parent.prepend(node);
+            });
+        });
+
+        return this;
     }
 
-    if (typeof selector === "number") {
-      if (!this._nodes[selector]) {
-        return new E();
-      }
+    clone() {
+        if (!this._nodes.length) {
+            return this;
+        }
 
-      return new E(this._nodes[selector]);
+        let clones = [];
+
+        this._nodes.forEach((node) => {
+            clones.push(node.cloneNode(true));
+        });
+
+        return new E(clones);
     }
 
-    return this;
-  }
+    remove() {
+        if (!this._nodes.length) {
+            return this;
+        }
 
-  find(selector) {
-    if (!this._nodes.length) {
-      return this;
+        this._nodes.forEach((node) => {
+            node.parentNode.removeChild(node);
+        });
     }
 
-    if (!selector) {
-      return this;
+    /**
+     * Classes
+     */
+    addClass(className) {
+        if (!this._nodes.length) {
+            return this;
+        }
+
+        if (!className) {
+            return this;
+        }
+
+        this._nodes.forEach((node) => {
+            node.classList.add(className);
+        });
+
+        return this;
     }
 
-    return new E(this._nodes[0].querySelectorAll(selector));
-  }
+    removeClass(className) {
+        if (!this._nodes.length) {
+            return this;
+        }
 
-  closest(selector) {
-    if (!this._nodes.length) {
-      return this;
+        if (!className) {
+            return this;
+        }
+
+        this._nodes.forEach((node) => {
+            node.classList.remove(className);
+        });
+
+        return this;
     }
 
-    if (!selector) {
-      return this;
+    toggleClass(className) {
+        if (!this._nodes.length) {
+            return this;
+        }
+
+        if (!className) {
+            return this;
+        }
+
+        this._nodes.forEach((node) => {
+            if (node.classList.contains(className)) {
+                node.classList.remove(className);
+            } else {
+                node.classList.add(className);
+            }
+        });
+
+        return this;
     }
 
-    return new E(this._nodes[0].closest(selector));
-  }
+    /**
+     * Attributes
+     */
+    setAttr(key, value, namespace) {
+        if (!this._nodes.length) {
+            return this;
+        }
 
-  /**
-   * Manipulating
-   */
-  append(children) {
-    if (!this._nodes.length) {
-      return this;
+        if (!key || !value) {
+            return this;
+        }
+
+        if (namespace) {
+            key = 'data-' + namespace + '-' + key;
+        }
+
+        this._nodes.forEach((node) => {
+            node.setAttribute(key, value);
+        });
+
+        return this;
     }
 
-    if (!children) {
-      return this;
+    getAttr(key, namespace) {
+        if (!this._nodes.length) {
+            return this;
+        }
+
+        if (!key) {
+            return null;
+        }
+
+        if (namespace) {
+            key = 'data-' + namespace + '-' + key;
+        }
+
+        return this._nodes[0].getAttribute(key);
     }
 
-    children = new E(children);
+    toggleAttr(key, values = [], namespace) {
+        if (!this._nodes.length) {
+            return this;
+        }
 
-    if (!children) {
-      return this;
+        if (!values || !values.length || values.length !== 2) {
+            return this;
+        }
+
+        if (namespace) {
+            key = 'data-' + namespace + '-' + key;
+        }
+
+        this._nodes.forEach((node) => {
+            if (node.getAttribute(key) === values[1]) {
+                node.setAttribute(key, values[0]);
+            } else {
+                node.setAttribute(key, values[1]);
+            }
+        });
+
+        return this;
     }
 
-    this._nodes.forEach((node) => {
-      children.nodes().forEach((child) => {
-        node.append(child);
-      });
-    });
+    /**
+     * Text
+     */
+    setText(text) {
+        if (!this._nodes.length) {
+            return this;
+        }
 
-    return this;
-  }
+        this._nodes.forEach((node) => {
+            node.textContent = text;
+        });
 
-  appendTo(parents) {
-    if (!this._nodes.length) {
-      return this;
+        return this;
     }
 
-    if (!parents) {
-      return this;
+    getText() {
+        if (!this._nodes.length) {
+            return this;
+        }
+
+        return this._nodes[0].textContent;
     }
 
-    parents = new E(parents);
+    /**
+     * HTML
+     */
+    setHTML(html) {
+        if (!this._nodes.length) {
+            return this;
+        }
 
-    if (!parents) {
-      return this;
+        this._nodes.forEach((node) => {
+            node.innerHTML = html;
+        });
+
+        return this;
     }
 
-    this._nodes.forEach((node) => {
-      parents.nodes().forEach((parent) => {
-        parent.append(node);
-      });
-    });
+    getHTML() {
+        if (!this._nodes.length) {
+            return this;
+        }
 
-    return this;
-  }
-
-  prepend(children) {
-    if (!this._nodes.length) {
-      return this;
+        return this._nodes[0].innerHTML;
     }
 
-    if (!children) {
-      return this;
+    /**
+     * CSS
+     */
+    css(styles) {
+        if (!this._nodes.length) {
+            return this;
+        }
+
+        if (!styles || typeof styles !== 'object') {
+            return this;
+        }
+
+        this._nodes.forEach((node) => {
+            for (const property in styles) {
+                node.style[property] = styles[property];
+            }
+        });
+
+        return this;
     }
 
-    children = new E(children);
+    /**
+     * Helper
+     */
+    repaint() {
+        if (!this._nodes.length) {
+            return this;
+        }
 
-    if (!children) {
-      return this;
+        this._nodes.forEach((node) => {
+            node.offsetHeight;
+        });
+
+        return this;
     }
 
-    this._nodes.forEach((node) => {
-      children.nodes().forEach((child) => {
-        node.prepend(child);
-      });
-    });
-
-    return this;
-  }
-
-  prependTo(parents) {
-    if (!this._nodes.length) {
-      return this;
+    isEmpty() {
+        if (!this._nodes.length) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    if (!parents) {
-      return this;
+    /**
+     * Get nodes
+     */
+    nodes(index) {
+        if (!this._nodes.length) {
+            return [];
+        }
+
+        if (index !== undefined) {
+            return this._nodes[index];
+        } else {
+            return this._nodes;
+        }
     }
 
-    parents = new E(parents);
-
-    if (!parents) {
-      return this;
+    n(index) {
+        return this.nodes(index);
     }
 
-    this._nodes.forEach((node) => {
-      parents.nodes().forEach((parent) => {
-        parent.prepend(node);
-      });
-    });
-
-    return this;
-  }
-
-  clone() {
-    if (!this._nodes.length) {
-      return this;
+    get(index) {
+        return this.nodes(index);
     }
-
-    let clones = [];
-
-    this._nodes.forEach((node) => {
-      clones.push(node.cloneNode(true));
-    });
-
-    return new E(clones);
-  }
-
-  remove() {
-    if (!this._nodes.length) {
-      return this;
-    }
-
-    this._nodes.forEach((node) => {
-      node.parentNode.removeChild(node);
-    });
-  }
-
-  /**
-   * Classes
-   */
-  addClass(className) {
-    if (!this._nodes.length) {
-      return this;
-    }
-
-    if (!className) {
-      return this;
-    }
-
-    this._nodes.forEach((node) => {
-      node.classList.add(className);
-    });
-
-    return this;
-  }
-
-  removeClass(className) {
-    if (!this._nodes.length) {
-      return this;
-    }
-
-    if (!className) {
-      return this;
-    }
-
-    this._nodes.forEach((node) => {
-      node.classList.remove(className);
-    });
-
-    return this;
-  }
-
-  toggleClass(className) {
-    if (!this._nodes.length) {
-      return this;
-    }
-
-    if (!className) {
-      return this;
-    }
-
-    this._nodes.forEach((node) => {
-      if (node.classList.contains(className)) {
-        node.classList.remove(className);
-      } else {
-        node.classList.add(className);
-      }
-    });
-
-    return this;
-  }
-
-  /**
-   * Attributes
-   */
-  setAttr(key, value, namespace) {
-    if (!this._nodes.length) {
-      return this;
-    }
-
-    if (!key || !value) {
-      return this;
-    }
-
-    if (namespace) {
-      key = "data-" + namespace + "-" + key;
-    }
-
-    this._nodes.forEach((node) => {
-      node.setAttribute(key, value);
-    });
-
-    return this;
-  }
-
-  getAttr(key, namespace) {
-    if (!this._nodes.length) {
-      return this;
-    }
-
-    if (!key) {
-      return null;
-    }
-
-    if (namespace) {
-      key = "data-" + namespace + "-" + key;
-    }
-
-    return this._nodes[0].getAttribute(key);
-  }
-
-  toggleAttr(key, values = [], namespace) {
-    if (!this._nodes.length) {
-      return this;
-    }
-
-    if (!values || !values.length || values.length !== 2) {
-      return this;
-    }
-
-    if (namespace) {
-      key = "data-" + namespace + "-" + key;
-    }
-
-    this._nodes.forEach((node) => {
-      if (node.getAttribute(key) === values[1]) {
-        node.setAttribute(key, values[0]);
-      } else {
-        node.setAttribute(key, values[1]);
-      }
-    });
-
-    return this;
-  }
-
-  /**
-   * Text
-   */
-  setText(text) {
-    if (!this._nodes.length) {
-      return this;
-    }
-
-    this._nodes.forEach((node) => {
-      node.textContent = text;
-    });
-
-    return this;
-  }
-
-  getText() {
-    if (!this._nodes.length) {
-      return this;
-    }
-
-    return this._nodes[0].textContent;
-  }
-
-  /**
-   * HTML
-   */
-  setHTML(html) {
-    if (!this._nodes.length) {
-      return this;
-    }
-
-    this._nodes.forEach((node) => {
-      node.innerHTML = html;
-    });
-
-    return this;
-  }
-
-  getHTML() {
-    if (!this._nodes.length) {
-      return this;
-    }
-
-    return this._nodes[0].innerHTML;
-  }
-
-  /**
-   * CSS
-   */
-  css(styles) {
-    if (!this._nodes.length) {
-      return this;
-    }
-
-    if (!styles || typeof styles !== "object") {
-      return this;
-    }
-
-    this._nodes.forEach((node) => {
-      for (const property in styles) {
-        node.style[property] = styles[property];
-      }
-    });
-
-    return this;
-  }
-
-  /**
-   * Helper
-   */
-  repaint() {
-    if (!this._nodes.length) {
-      return this;
-    }
-
-    this._nodes.forEach((node) => {
-      node.offsetHeight;
-    });
-
-    return this;
-  }
-
-  isEmpty() {
-    if (!this._nodes.length) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  /**
-   * Get nodes
-   */
-  nodes(index) {
-    if (!this._nodes.length) {
-      return [];
-    }
-
-    if (index !== undefined) {
-      return this._nodes[index];
-    } else {
-      return this._nodes;
-    }
-  }
-
-  n(index) {
-    return this.nodes(index);
-  }
-
-  get(index) {
-    return this.nodes(index);
-  }
 }
